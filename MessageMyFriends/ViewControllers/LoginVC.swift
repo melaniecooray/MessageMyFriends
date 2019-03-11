f8fb07556d4ea04ebb8b8774218c1e1e88d57b65
Aadhrik Kuila <aadhrik.kuila@berkeley.edu>
2019-03-11 16:47:50 -0700

    fixed login

--- MessageMyFriends/ViewControllers/LoginVC.swift
+++ MessageMyFriends/ViewControllers/LoginVC.swift
@@ -11,7 +11,6 @@ import UIKit
 class LoginVC: UIViewController {
     
     var titleLabel : UILabel!
-    var backButton : UIButton!
     var email : UITextField!
     var submitButton : UIButton!
     
@@ -30,18 +29,8 @@ class LoginVC: UIViewController {
         titleLabel.font = UIFont(name: "Avenir", size: 30)
         view.addSubview(titleLabel)
         
-        backButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.width/5, height: 30))
-        backButton.center = CGPoint(x: view.frame.width/5, y: view.frame.height/8)
-        backButton.setTitle("< Back", for: .normal)
-        backButton.setTitleColor(UIColor.white, for: .normal)
-        backButton.titleLabel?.font = UIFont(name: "Avenir", size: 20)
-        backButton.backgroundColor = .black
-        backButton.layer.cornerRadius = 8
-        backButton.addTarget(self, action: #selector(backToLaunchVC), for: .touchUpInside)
-        view.addSubview(backButton)
-        
-        email = UITextField(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
-        email.center = CGPoint(x: view.frame.width/2, y: view.frame.height/3 * 2)
+        email = UITextField(frame: CGRect(x: 0, y: 0, width: view.frame.width/2, height: 50))
+        email.center = CGPoint(x: view.frame.width/2, y: view.frame.height/2)
         email.placeholder = "Email"
         email.textColor = .black
         email.autocapitalizationType = UITextAutocapitalizationType.none
@@ -49,7 +38,7 @@ class LoginVC: UIViewController {
         view.addSubview(email)
         
         submitButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.width/2, height: 50))
-        submitButton.center = CGPoint(x: view.frame.width/2, y: view.frame.height/5 * 4)
+        submitButton.center = CGPoint(x: view.frame.width/2, y: view.frame.height/3 * 2)
         submitButton.setTitle("Login", for: .normal)
         submitButton.setTitleColor(UIColor.white, for: .normal)
         submitButton.backgroundColor = .black
@@ -57,10 +46,6 @@ class LoginVC: UIViewController {
         submitButton.layer.cornerRadius = 14
         //submitButton.addTarget(self, action: #selector(openLoginVC), for: .touchUpInside)
         view.addSubview(submitButton)
-    }
-    
-    @objc func backToLaunchVC() {
-        performSegue(withIdentifier: "toLaunchVC", sender: self)
     }
     
 }
